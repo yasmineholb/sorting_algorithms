0fa1da9584f7ea0333e00c75bb01a68d971594bc
yasmineholb <1055@holbertonschool.com>
2020-02-03 19:44:37 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ *bubble_sort - fn
+ *@array: array
+ *@size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t verif, i, test = 0;
+verif = size;
+if (!array || !size)
+return;
+while (verif > 0)
+{
+for (i = 0; i < size - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+test = array[i + 1];
+array[i + 1] = array[i];
+array[i] = test;
+}
+print_array(array, size);
+}
+verif--;
+}
+}
+
