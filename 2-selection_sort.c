f3480f109b51e106174d50916f913cb66b951fb4
yasmineholb <1055@holbertonschool.com>
2020-02-03 18:25:03 +0100

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+/**
+ *
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+  int temp;
+  size_t i, j;
+    for(i=0;i<size;i++){
+      for(j=i+1;j<size;j++){
+         if(array[i]>array[j]){
+            temp=array[i];
+            array[i]=array[j];
+            array[j]=temp;
+	     
+         }
+	 
+      }
+       print_array(array, size);
+    }
+   
+}
