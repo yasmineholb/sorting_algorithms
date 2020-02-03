9d8c3b4637cde182217508086c67738d38c4acb5
yasmineholb <1055@holbertonschool.com>
2020-02-03 20:05:12 +0100

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ *insertion_sort_list - fn
+ *@list: list
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *m, *s;
+if (!list || !(*list) || !(*list)->next)
+return;
+m = *list;
+while (m)
+{
+s = m;
+while (s->prev && s->n < s->prev->n)
+{
+s->prev->next = s->next;
+if (s->next)
+{
+s->next->prev = s->prev;
+}
+s->next = s->prev;
+s->prev = s->prev->prev;
+s->next->prev = s;
+if (!s->prev)
+*list = s;
+else
+s->prev->next = s;
+}
+m = m->next;
+print_list(*list);
+}
+}
