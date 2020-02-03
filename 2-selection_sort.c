5b660811ec14b5d1029d6a99e0cb8aaa3a82c6a7
yasmineholb <1055@holbertonschool.com>
2020-02-03 18:36:31 +0100

    fix selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,23 +1,24 @@
 #include "sort.h"
 /**
- *
- *
+ *selection_sort - fn
+ *@array: array
+ *@size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
-  int temp;
-  size_t i, j;
-    for(i=0;i<size;i++){
-      for(j=i+1;j<size;j++){
-         if(array[i]>array[j]){
-            temp=array[i];
-            array[i]=array[j];
-            array[j]=temp;
-	     
-         }
-	 
-      }
-       print_array(array, size);
-    }
-   
+int temp;
+size_t i, j;
+for (i = 0; i < size; i++)
+{
+for (j = i + 1; j < size; j++)
+{
+if (array[i] > array[j])
+{
+temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+}
+}
+print_array(array, size);
+}
 }
