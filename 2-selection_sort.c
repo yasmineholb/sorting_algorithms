0ff871be0e0d32226625417244f0ee0438cd87df
yasmineholb <1055@holbertonschool.com>
2020-02-03 19:12:41 +0100

    modifing fn

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,18 +7,21 @@
 void selection_sort(int *array, size_t size)
 {
 int temp;
-size_t i, j;
+size_t i, j, m;
 if (!array || !size)
 return;
 for (i = 0; i < size; i++)
 {
+m = i;
 for (j = i + 1; j < size; j++)
 {
-if (array[i] > array[j])
+if (array[m] > array[j])
+m = j;
+if (i != m)
 {
 temp = array[i];
-array[i] = array[j];
-array[j] = temp;
+array[i] = array[m];
+array[m] = temp;
 }
 }
 print_array(array, size);
