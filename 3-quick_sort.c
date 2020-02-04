c757c0b7067434eb56691187db102f95d345bbaf
yasmineholb <1055@holbertonschool.com>
2020-02-04 02:20:45 +0100

    fix quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 /**
- *
- *
+ *quick_sort - fn
+ *@array: array
+ *@size: array size
  */
 
 void quick_sort(int *array, size_t size)
@@ -12,9 +13,33 @@ sortone(array, 0, size - 1, size);
 }
 
 /**
- *
- *
+ *sortone - fn
+ *@array: array
+ *@f: first element
+ *@fn: last element
+ *@size: size of array
  */
+
+void sortone(int *array, size_t f, size_t fn, size_t size)
+{
+size_t k;
+if (fn == 0 || fn <= f)
+return;
+k = divide(array, f, fn, size);
+if (k != 0 && k > f)
+sortone(array, f, k - 1, size);
+if (k < size - 1)
+sortone(array, k + 1, fn, size);
+}
+/**
+ *divide - fn
+ *@array: array
+ *@f: first element
+ *@fn: last element
+ *@size: size of array
+ *Return: size_t
+ */
+
 size_t divide(int *array, size_t f, size_t fn, size_t size)
 {
 int tmp, p;
@@ -42,22 +67,6 @@ fn = f;
 print_array(array, size);
 }
 return (fn);
-}
-/**
- *
- *
- */
-
-void sortone(int *array, size_t f, size_t fn, size_t size)
-{
-size_t k;
-if (fn == 0 || fn <= f)
-return;
-k = divide(array, f, fn, size);
-if (k != 0 && k > f)
-sortone(array, f, k - 1, size);
-if (k < size - 1)
-sortone(array, k + 1, fn, size);
 }
 
 
